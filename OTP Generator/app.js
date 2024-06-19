4e3c6811e8cc02a96caf4f328bd29c6eb0a3dd1c
arafatmollah <arafatshuvo18@gmail.com>
2024-06-19 22:14:01 +0600

    OTP generator app

--- OTP Generator/app.js
+++ OTP Generator/app.js
@@ -1,6 +1,8 @@
 let generate;
 let successMessage = document.getElementById('otp-validation-success');
-let failMessage = document.getElementById('otp-validation-fail')
+let failMessage = document.getElementById('otp-timing')
+let otpExpire = document.getElementById('otp-expire')
+
 
 function tackleInput(){
     const input = document.getElementById('input-field');
@@ -28,6 +30,8 @@ function generateOtp(){
     generate = Math.floor(1000+Math.random()*9000)
     let generatedOtp = document.getElementById('generated-otp');
     generatedOtp.innerText = generate
+
+    otpManage()
 }
 
 
@@ -43,12 +47,35 @@ function validation(){
 
     if(result){
         successMessage.innerText = 'Your Otp is matched'
-        successMessage.classList.remove=(failMessage)
+        successMessage.classList.remove("fail");
+        successMessage.classList.add("success")
+        // successMessage.classList.remove=(failMessage)
     }
     else{
-        failMessage.innerText = 'Your otp matched failed'
+        successMessage.innerText = 'Your otp matched failed'
+        successMessage.classList.remove('success');
+        successMessage.classList.add('fail')
     }
 }
+
+function otpManage(){
+    let timeout = 15000;
+    let interval = 1000;
+    let slice = timeout/interval;
+
+    let setInt = setInterval(function (){
+        otpExpire.innerText = `Time will expire in ${slice}`
+        slice = slice-1;
+    },interval)
+
+    setTimeout(function (){
+        otpExpire.innerText = `OTP Expire`;
+        clearInterval(otpManage)
+        generateOtp();
+    }, timeout)
+
+}
+
 
 
 function init(){
