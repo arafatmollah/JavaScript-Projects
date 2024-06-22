63e00aaf1bf598b46e175425cced2bf8757f7acb
arafatmollah <arafatshuvo18@gmail.com>
2024-06-22 20:45:08 +0600

    first part done

--- Movie Info/src/index.js
+++ Movie Info/src/index.js
@@ -7,15 +7,32 @@ function init(){
 
 function paintStatistics(movieReviews){
     let flatmovieReviews = movieReviews.flat();
-    let totalMovie = flatmovieReviews.length;
+    let totalMovie = movieReviews.length;
     let totalReviews = flatmovieReviews.length;
-    let totalRating = flatmovieReviews.reducue((arc, item)=>{
-        return arc + item;
+   
+    let totalRating = flatmovieReviews.reduce((arc, item)=>{
+        return arc + item.rating;
     }, 0);
+   
 
     let averageRating = (totalRating/totalReviews).toFixed(2)
 
+    let tMovies = document.getElementById('tMovie')
+    addStats(tMovies, totalMovie)
+    let averageReviews = document.getElementById('tAverage');
+    addStats(averageReviews,averageRating )
+    let tRatings = document.getElementById('tRating')
+    addStats(tRatings, totalReviews)
 }
+
+function addStats(elem, value){
+    let spanEl = document.createElement("span");
+    spanEl.classList.add('3xl')
+    spanEl.innerText = value;
+    elem.appendChild(spanEl)
+}
+
+
 
 
 init();
